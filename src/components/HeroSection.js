cd26645fba37228659cc08e8eadfe15e6cad13fd
Funakoshi Silva <funakoshisilva@Funakoshis-MacBook-Pro.local>
2023-08-14 19:41:57 +0100

    removed blanck target

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -60,7 +60,7 @@ function HeroSection(){
                             <div className="flex justify-center md:justify-start">
                                 <a className="mr-2 p-3 px-6 pt-2 text-white inline-block rounded-full bg-cyan-500 hover:bg-blue-500 hover:shadow-lg transform transition hover:-translate-y-1 focus:ring-2
                                     focus:ring-blue-600 ring-offset-2 outline-none focus:bg-blue-800
-                                    focus:shadow-lg active:bg-blue-900 md:mr-5" href={cv} target = "_blank">
+                                    focus:shadow-lg active:bg-blue-900 md:mr-5" href={cv} >
                                     Currículo
                                 </a>
  
