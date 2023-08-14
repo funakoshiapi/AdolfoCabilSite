378b7be4d6c6ac59d8784cd32bbfbfab97cc0e74
Funakoshi Silva <funakoshisilva@Funakoshis-MacBook-Pro.local>
2023-08-14 19:47:56 +0100

    removed blank target and added alt

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,7 +7,7 @@ function NavBar(){
           <div className='shadow-sm bg-gray-80 px-10'>
           <nav className="py-5 mb-2 flex justify-between ">
             <div className='flex'>
-              <img className='mr-5' src={logo} width="40" height="4" />
+              <img className='mr-5' src={logo} width="40" height="4" alt='logo' />
               <button className='text-3xl font-bold font-montserrat  ' > Adolfo Cabeia</button>
             </div>
 
@@ -15,7 +15,7 @@ function NavBar(){
             <li>
             <a className="mr-2 p-3 px-6 pt-2 text-white inline-block rounded-full bg-cyan-500 hover:bg-blue-500 hover:shadow-lg transform transition hover:-translate-y-1 focus:ring-2
                                     focus:ring-blue-600 ring-offset-2 outline-none focus:bg-blue-800
-                                    focus:shadow-lg active:bg-blue-900 md:mr-5" href={cv} target = "_blank">
+                                    focus:shadow-lg active:bg-blue-900 md:mr-5" href={cv} >
                                     Currículo
             </a>
             </li>
