8558857fc4d4fc760057d4831c843979cd7d2261
Funakoshi Silva <funakoshisilva@Funakoshis-MacBook-Pro.local>
2023-08-18 21:45:38 +0100

    fixed style

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,7 +11,7 @@ const Contact = () => {
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             Contact
           </p>
-          <p className="py-6">Submit the form below to get in touch with me</p>
+          <p className="py-6">Entre em contacto comigo</p>
         </div>
 
         <div className=" flex justify-center items-center">
@@ -23,24 +23,24 @@ const Contact = () => {
             <input
               type="text"
               name="name"
-              placeholder="Enter your name"
+              placeholder="Nome"
               className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
             />
             <input
               type="text"
               name="email"
-              placeholder="Enter your email"
+              placeholder="Email"
               className="my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
             />
             <textarea
               name="message"
-              placeholder="Enter your message"
+              placeholder="Mensagem"
               rows="10"
               className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
             ></textarea>
 
             <button className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
-              Let's talk
+              Contactar
             </button>
           </form>
         </div>
